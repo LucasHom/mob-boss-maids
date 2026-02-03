5d4ad4a88dcc31e9a6a55043c347893290c64eab
Lucas Hom <lucasrylanhom@gmail.com>
2026-02-03 02:24:08 -0800

    start scene done

--- Assets/Scripts/Page.cs
+++ Assets/Scripts/Page.cs
@@ -1,40 +1,104 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class Page : MonoBehaviour
 {
     [SerializeField] private Transform splurts;
 
     private bool gameStarting = false;
+    private bool splurtsCleared = false;
+    private bool grabbed = false;
+    private int timesGrabbed = 0;
+
+    private XRGrabInteractable grabInteractable;
+
+    [SerializeField] private InputActionProperty aButton;
+
+    void Awake()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.selectEntered.AddListener(OnGrabbed);
+        grabInteractable.selectExited.AddListener(OnReleased);
+    }
+
+
 
     private void Start()
     {
         // Start the coroutine that checks every 1 second
         StartCoroutine(CheckChildrenRoutine());
+        splurtsCleared = false;
+        gameStarting = false;
+        grabbed = false;
+        timesGrabbed = 0;
+    }
+
+    private void OnGrabbed(SelectEnterEventArgs args)
+    {
+        grabbed = true;
+        timesGrabbed++; 
+    }
+
+    private void OnReleased(SelectExitEventArgs args)
+    {
+        grabbed = false;
     }
 
     private IEnumerator CheckChildrenRoutine()
     {
-        while (!gameStarting)
+        while (true)
         {
             yield return new WaitForSeconds(1f);
-            // Check if splurts has 4 children
-            Debug.Log("Checking children: " + splurts.childCount);
-            if (splurts.childCount == 4)
+
+            bool anySplurts = false;
+
+            foreach (Transform child in splurts)
             {
-                gameStarting = true;
-                // Start the coroutine that waits 5 seconds and then loads the scene
-                StartCoroutine(StartGame());
+                if (child.name.Contains("Splurt"))
+                {
+                    anySplurts = true;
+                    break;
+                }
+            }
+
+            if (!anySplurts)
+            {
+                Debug.Log("All Splurts cleared!");
+                splurtsCleared = true;
+                yield break; // stop the coroutine immediately
             }
         }
     }
 
+    private void Update()
+    {
+        if (grabbed && !gameStarting && splurtsCleared)
+        {
+            gameStarting = true;
+            StartCoroutine(StartGame());
+        }
+
+        if (timesGrabbed > 5)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+
+    }
+
     private IEnumerator StartGame()
     {
-        Debug.Log("Starting");
-        yield return new WaitForSeconds(10f); // wait 5 seconds
-        Debug.Log("Loading scene now");
-        SceneManager.LoadScene("MainScene"); // load the scene AFTER wait
+        SFXManager.Instance.PlaySFX("eyluigi");
+        yield return new WaitForSeconds(15f);
+        SceneManager.LoadScene("MainScene");
+    }
+
+    void OnDestroy()
+    {
+        grabInteractable.selectEntered.RemoveListener(OnGrabbed);
+        grabInteractable.selectExited.RemoveListener(OnReleased);
     }
 }
